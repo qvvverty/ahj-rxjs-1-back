693b7d59b81a0c5db877554e39ff9787e94297fb
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-08-01 19:15:36 +0300

    ready to deploy

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const http = require('http');
 const Koa = require('koa');
 const Router = require('koa-router');
 const Message = require('./Message');
-// const koaBody = require('koa-body');
 
 const port = process.env.PORT || 7070;
 const app = new Koa();
 const router = new Router();
-
-// app.use(koaBody({
-//   multipart: true,
-// }));
 
 function getRandomNumber(min, max) {
   const randomNumber = min + Math.random() * (max + 1 - min);
@@ -20,31 +15,14 @@ function getRandomNumber(min, max) {
 app
   .use(async (ctx, next) => {
     ctx.response.set({
-      // 'Access-Control-Allow-Origin': 'https://qvvverty.github.io',
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'https://qvvverty.github.io',
+      // 'Access-Control-Allow-Origin': '*',
     });
     await next();
   })
   .use(router.routes());
 
-// app.use(async (ctx) => {
-//   ctx.response.body = "I'm running";
-// ctx.response.body = faker.lorem.sentences();
-// ctx.response.body = uuidv4();
-
-// if (!users.includes(ctx.request.body)) {
-//   ctx.response.status = 200;
-//   ctx.response.body = JSON.stringify(users);
-//   users.push(ctx.request.body);
-// } else {
-//   ctx.response.status = 403;
-// }
-// });
-
 router.get('/messages/unread', (ctx) => {
-  // ctx.response.body = 'router working';
-  // ctx.response.body = JSON.stringify(new Message());
-
   const responseObj = {
     status: 'ok',
     timestamp: Date.now(),
